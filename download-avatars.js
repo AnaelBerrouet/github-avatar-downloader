01d41dc56f35453ddfbf99916b1707d2cb8fbeab
AnaelBerrouet <anael.jberrouet@gmail.com>
2018-10-01 18:35:59 +0000

    added getRepContributors function in download-avatar.js

---
+++ download-avatars.js
@@ -0,0 +1,48 @@
+var request = require('request');
+var token = require('./secrets.js')['GITHUB_TOKEN'];
+
+
+console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+
+  let options = {
+    url : "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      Authorization: token
+    }
+  };
+
+  request(options, function(err, res, body) {
+
+    cb(err, body);
+
+  });
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
+
+// const fs = require('fs');
+// const request = require('request');
+
+// const URL = 'https://sytantris.github.io/http-examples/future.jpg';
+
+// request.get(URL)
+//   .on('err', function(err) {
+//     console.log(err);
+//   })
+//   .on('response', function(response) {
+//     console.log("Response status:",response.statusCode);
+//     if(response.statusCode===200){
+//       console.log("Downloading...");
+//     }
+//   })
+//   .pipe(fs.createWriteStream('./future.jpg')
+//     .on('finish', function() {
+//     console.log("Download complete.");
+//   }));
